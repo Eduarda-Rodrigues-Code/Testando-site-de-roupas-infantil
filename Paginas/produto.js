890342f0c0344958f958eb42bb2d4ffd0543482b
eduarda rodrigues oliveira <eduardarod04@gmail.com>
2025-06-30 13:34:32 -0300

    v1

---
+++ Paginas/produto.js
@@ -0,0 +1,51 @@
+const produtos = [
+  {
+    id: 1,
+    categoria: "menino",
+    nome: "Conjunto De Linho Azul",
+    preco: "R$ 160,00",
+    imagens: [
+      "img/portifolioMasc/1.png",
+      "img/INDEX/card1.png",
+      "img/INDEX/card2.png"
+    ],
+    cores: ["#000000", "#1e1685", "#2a640b"],
+    material: "Linho 100%",
+    tamanhos: ["P", "M", "G"],
+    descricao: "Conjunto elegante e leve para dias quentes."
+  },
+  {
+    id: 2,
+    categoria: "menino",
+    nome: "Conjunto Casual Verde",
+    preco: "R$ 140,00",
+    imagens: [
+      "img/portifolioMasc/2.png",
+      "img/INDEX/card1.png",
+      "img/INDEX/card2.png"
+    ],
+    cores: ["#1e1685"],
+    material: "Algodão 100%",
+    tamanhos: ["P", "M", "G"],
+    descricao: "Conjunto confortável para uso diário."
+  },
+  {
+    id: 3,
+    categoria: "menino",
+    nome: "Camisa Polo Vermelha",
+    preco: "R$ 90,00",
+    imagens: [
+      "img/portifolioMasc/3.png",
+      "img/INDEX/card1.png",
+      "img/INDEX/card2.png"
+    ],
+    cores: ["#d32f2f"],
+    material: "Algodão 100%",
+    tamanhos: ["P", "M", "G", "GG"],
+    descricao: "Camisa polo clássica, perfeita para ocasiões informais."
+  }
+{ id: 2, 
+    nome: "Conjunto De Linho",
+     preco: "R$ 160,00", 
+     imagens: [ "img/portifolioMasc/2.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: ["#1e1685"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }, 
+{ id: 3, nome: "Conjunto De Linho", preco: "R$ 160,00", imagens: [ "img/portifolioMasc/3.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: [ "#00793c"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }, { id: 4, nome: "Conjunto De Linho", preco: "R$ 160,00", imagens: [ "img/portifolioMasc/4.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: ["#000000", "#1e1685", "#c57000"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }, { id: 5, nome: "Conjunto De Linho", preco: "R$ 160,00", imagens: [ "img/portifolioMasc/5.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: ["#000000"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }, { id: 6, nome: "Conjunto De Linho", preco: "R$ 160,00", imagens: [ "img/portifolioMasc/6.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: ["#000000;"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }, { id: 7, nome: "Conjunto De Linho", preco: "R$ 160,00", imagens: [ "img/portifolioMasc/7.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: ["#d8c4aa", "#00793c"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }, { id: 8, nome: "Conjunto De Linho", preco: "R$ 160,00", imagens: [ "img/portifolioMasc/8.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: ["#000000;", "#1e1685"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }, { id: 9, nome: "Conjunto De Linho", preco: "R$ 160,00", imagens: [ "img/portifolioMasc/9.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: ["#c57000"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }, { id: 10, nome: "Conjunto De Linho", preco: "R$ 160,00", imagens: [ "img/portifolioMasc/10.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: ["#000000", "#00793c"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }, { id: 11, nome: "Conjunto De Linho", preco: "R$ 160,00", imagens: [ "img/portifolioMasc/11.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: ["#d8c4aa", "#1e1685"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }, { id: 12, nome: "Conjunto De Linho", preco: "R$ 160,00", imagens: [ "img/portifolioMasc/12.png", "img/INDEX/card1.png", "img/INDEX/card2.png" ], cores: [ "#00793c"], material: "Linho 100%", tamanhos: ["P", "M", "G"], descricao: "Conjunto elegante e leve para dias quentes." }];
